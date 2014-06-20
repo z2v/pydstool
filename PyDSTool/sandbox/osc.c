56e768ee3b6c223d2d32fede07e66d89fd9d62c4
Vladimir Zakharov <zakharov.vv@gmail.com>
2014-06-20 10:23:55 +0400

    Extract integration in C version, add running script
    [ci skip]

--- PyDSTool/sandbox/osc.c
+++ PyDSTool/sandbox/osc.c
@@ -12,50 +12,57 @@ void oscillator(unsigned n, double x, double* y, double* p, double* f) {
 }
 
 
+int run(unsigned n, FcnEqDiff vf, double* y0, double t0, double t1) {
+    double rtol = 1e-6;
+    double atol = 1e-12;
+
+    return dop853(
+        2,          /* dimension of the system <= UINT_MAX-1*                                                             */
+        oscillator, /* function computing the value of f(x,y) *                                                           */
+        t0,         /* initial x-value *                                                                                  */
+        y0,         /* initial values for y *                                                                             */
+        NULL,       /* vector field parameters *                                                                          */
+        t1,         /* final x-value (xend-x may be positive or negative) *                                               */
+        &rtol,      /* relative error tolerance *                                                                         */
+        &atol,      /* absolute error tolerance *                                                                         */
+        0,          /* switch for rtoler and atoler *                                                                     */
+        NULL,       /* function providing the numerical solution during integration *                                     */
+        0,          /* switch for calling solout *                                                                        */
+        NULL,       /* messages stream *                                                                                  */
+        0.0,        /* rounding unit *                                                                                    */
+        0.9,        /* safety factor *                                                                                    */
+        0.3,        /* parameters for step size selection *                                                               */
+        6.0,
+        0.0,        /* for stabilized step size control *                                                                 */
+        0.0,        /* maximal step size *                                                                                */
+        0.0,        /* initial step size *                                                                                */
+        1e6,        /* maximal number of allowed steps *                                                                  */
+        1,          /* switch for the choice of the coefficients *                                                        */
+        0,          /* test for stiffness *                                                                               */
+        2,          /* number of components for which dense outpout is required *                                         */
+        NULL,       /* indexes of components for which dense output is required, >= nrdens *                              */
+        0,          /* declared length of icon *                                                                          */
+        0,          /* Flag for checking magbound not exceeded: 0 no checking; 1 check for initial boundmaxsteps steps; * */
+        0,          /* number of initial steps for which to check magbound not exceeded *                                 */
+        NULL,       /* bound on absolute value of components of the solution *                                            */
+        NULL);      /* prevent h step from crossing external input times (if present) *                                   */
+}
+
+
 int main() {
     double y0[2];
     int status = 0;
-    double rtol = 1e-6;
-    double atol = 1e-12;
+    double t0 = 0.0;
+    double t1 = 1000.0;
     int i;
 
     struct timeval tv1, tv2;
     struct timezone tz1, tz2;
 
-
     y0[0] = 1.0;
     y0[1] = 0.1;
     gettimeofday(&tv1, &tz1);
-    status = dop853(
-            2,          /* dimension of the system <= UINT_MAX-1*                                                             */
-            oscillator, /* function computing the value of f(x,y) *                                                           */
-            0.0,        /* initial x-value *                                                                                  */
-            y0,         /* initial values for y *                                                                             */
-            NULL,       /* vector field parameters *                                                                          */
-            1000.0,     /* final x-value (xend-x may be positive or negative) *                                               */
-            &rtol,      /* relative error tolerance *                                                                         */
-            &atol,      /* absolute error tolerance *                                                                         */
-            0,          /* switch for rtoler and atoler *                                                                     */
-            NULL,       /* function providing the numerical solution during integration *                                     */
-            0,          /* switch for calling solout *                                                                        */
-            NULL,       /* messages stream *                                                                                  */
-            0.0,        /* rounding unit *                                                                                    */
-            0.9,        /* safety factor *                                                                                    */
-            0.3,        /* parameters for step size selection *                                                               */
-            6.0,
-            0.0,        /* for stabilized step size control *                                                                 */
-            0.0,        /* maximal step size *                                                                                */
-            0.0,        /* initial step size *                                                                                */
-            1e6,        /* maximal number of allowed steps *                                                                  */
-            1,          /* switch for the choice of the coefficients *                                                        */
-            0,          /* test for stiffness *                                                                               */
-            2,          /* number of components for which dense outpout is required *                                         */
-            NULL,       /* indexes of components for which dense output is required, >= nrdens *                              */
-            0,          /* declared length of icon *                                                                          */
-            0,          /* Flag for checking magbound not exceeded: 0 no checking; 1 check for initial boundmaxsteps steps; * */
-            0,          /* number of initial steps for which to check magbound not exceeded *                                 */
-            NULL,       /* bound on absolute value of components of the solution *                                            */
-            NULL);      /* prevent h step from crossing external input times (if present) *                                   */
+    status = run(2, oscillator, y0, t0, t1);
     gettimeofday(&tv2, &tz2);
     printf("Integration lasted %.3f milliseconds\n", ((tv2.tv_sec - tv1.tv_sec) * 1000000 + (tv2.tv_usec - tv1.tv_usec)) / 1000.);
 
---
+++ PyDSTool/sandbox/run.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import print_function
+
+import re
+import subprocess
+
+
+if subprocess.call(['cc', '-O2', 'osc.c', 'dop853/dop853.c', '-lm']) == 0:
+    times = []
+    nfun = []
+    for j in xrange(100):
+        out = subprocess.check_output(['./a.out'])
+        for l in out.split('\n'):
+            if 'milliseconds' in l:
+                times.append(float(re.findall(r'\d+.\d{3}', l)[0]))
+            if 'nfunc' in l:
+                nfun.append(int(re.findall(r'\d+', l)[0]))
+
+    print(sorted(zip(times, nfun))[0])
+
